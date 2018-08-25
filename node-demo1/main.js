f2027c27c721c6dcaa66a65b614c42385b818995
unknown <you@example.com>
2018-08-25 10:44:40 +0800

    add json

---
+++ node-demo1/main.js
@@ -0,0 +1,54 @@
+myBtn.addEventListener('click',(e)=>{
+  let request = new XMLHttpRequest()
+  request.open('GET','/xxx') //配置request
+  request.send()
+  request.onreadystatechange = ()=>{
+    if(request.readyState === 4){
+      console.log('请求响应完毕')
+      if(request.status >= 200 && request.status < 300){
+        console.log('请求成功')
+        // console.log(request.responseText)
+        let string = request.responseText
+        //把符合json语法的字符串转换成js对应的值
+        let object = window.JSON.parse(string) 
+        console.log(object)
+        console.log(object.note)
+        console.log(object.note.from)
+      }
+    }
+  }
+
+
+ 
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
